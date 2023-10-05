1af8de6022119cf791683dc08878c88dbd022441
Crane <15352461680@126.com>
2023-10-05 11:52:00 +0800

    修改了ReLU的反向传播计算

--- src/functions/ReLU.cuh
+++ src/functions/ReLU.cuh
@@ -15,6 +15,7 @@ class ReLU:public ActivationFunction<T>{
     virtual Tensor<T> forward(Tensor<T> &input);
     virtual Tensor<T> backward(Tensor<T> &input);
     virtual std::string getName();
+    Tensor<T> output;
 
 };
 
@@ -29,13 +30,15 @@ ReLU<T>::~ReLU(){}
 template<typename T>
 Tensor<T> ReLU<T>::forward(Tensor<T> &input){
     Tensor<T> output(input.getShape(), input.getDevice());
+    this->output = Tensor<T>(input.getShape(), input.getDevice());
     if(input.getDevice() == Device::CPU){
         for(int i = 0; i < input.getSize(); i++){
             output[i] = std::max((T)0, input[i]);
+            this->output[i] = output[i];
         }
     }else{
         reluForwardKernel<<<CudaGetBlocks(input.getSize()),kCudaThreadsNum>>>(
-            input.data_->data,output.data_->data,input.getSize());
+            input.data_->data,output.data_->data,input.getSize(),this->output.data_->data);
     }
     return output;
 }
@@ -44,11 +47,11 @@ Tensor<T> ReLU<T>::backward(Tensor<T> &grad){
     Tensor<T> output(grad.getShape(), grad.getDevice());
     if(grad.getDevice() == Device::CPU){
         for(int i = 0; i < grad.getSize(); i++){
-            output[i] = std::max((T)0, grad[i]);
+            output[i] = this->output[i] > 0 ? grad[i] : 0;
         }
     }else{
         reluBackwardKernel<<<CudaGetBlocks(grad.getSize()),kCudaThreadsNum>>>(
-            grad.data_->data,output.data_->data,grad.getSize());
+            grad.data_->data,output.data_->data,grad.getSize(),this->output.data_->data);
         
     }
     return output;
@@ -60,16 +63,17 @@ std::string ReLU<T>::getName(){
 }
 
 template <typename T>
-__global__ void reluForwardKernel(T* input,T* output,int size){
+__global__ void reluForwardKernel(T* input,T* output,int size,T* thisOutput){
     CUDA_KERNEL_LOOP(idx,size){
         output[idx] = max((T)0,input[idx]);
+        thisOutput[idx] = output[idx];
     }
 }
 
 template <typename T>
-__global__ void reluBackwardKernel(T* grad,T* output,int size){
+__global__ void reluBackwardKernel(T* grad,T* output,int size,T* thisOutput){
     CUDA_KERNEL_LOOP(idx,size){
-        output[idx] = max((T)0,grad[idx]);
+        output[idx] = thisOutput[idx] > 0 ? grad[idx] : 0;
     }
 }
 #endif // RELU_HPP
